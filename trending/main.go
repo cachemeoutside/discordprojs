b8532c8f7902bc0a8b6599370e67055f840fe6ed
Anthony Vuong <vuonga91@gmail.com>
2021-08-30 22:10:28 -0700

    Update go formatting

--- trending/main.go
+++ trending/main.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
-  for _, url := range getTrending() {
-    fmt.Printf("%s | %s\n", url, getAbout(url))
-  }
+	for _, url := range getTrending() {
+		fmt.Printf("%s | %s\n", url, getAbout(url))
+	}
 }
 
-func getTrending() []string{
-  var results []string
+func getTrending() []string {
+	var results []string
 	resp, _ := http.Get("https://github.com/trending")
 	tokenizer := html.NewTokenizer(resp.Body)
 	for {
@@ -33,39 +33,39 @@ func getTrending() []string{
 		case html.StartTagToken:
 			for _, a := range t.Attr {
 				if a.Key == "href" && strings.Contains(a.Val, "/stargazers") {
-          results = append(results, "https://github.com" + strings.ReplaceAll(a.Val, "/stargazers", ""))
+					results = append(results, "https://github.com"+strings.ReplaceAll(a.Val, "/stargazers", ""))
 				}
 			}
 		}
 	}
 
-  return results
+	return results
 }
 
 func getAbout(url string) string {
-  resp, _ := http.Get(url)
-  tokenizer := html.NewTokenizer(resp.Body)
-  for {
-    tt := tokenizer.Next()
-    t := tokenizer.Token()
-    err := tokenizer.Err()
-    if err == io.EOF {
-      break
-    }
+	resp, _ := http.Get(url)
+	tokenizer := html.NewTokenizer(resp.Body)
+	for {
+		tt := tokenizer.Next()
+		t := tokenizer.Token()
+		err := tokenizer.Err()
+		if err == io.EOF {
+			break
+		}
 
-    switch tt {
-    case html.ErrorToken:
-      log.Fatal(err)
-    case html.StartTagToken:
-      for _, a := range t.Attr {
-        if a.Key == "class" && a.Val == "f4 mt-3" {
-          tokenType := tokenizer.Next()
-          if tokenType == html.TextToken {
-            return strings.TrimSpace(tokenizer.Token().Data)
-          }
-        }
-      }
-    }
-  }
-  return "Description not available"
+		switch tt {
+		case html.ErrorToken:
+			log.Fatal(err)
+		case html.StartTagToken:
+			for _, a := range t.Attr {
+				if a.Key == "class" && a.Val == "f4 mt-3" {
+					tokenType := tokenizer.Next()
+					if tokenType == html.TextToken {
+						return strings.TrimSpace(tokenizer.Token().Data)
+					}
+				}
+			}
+		}
+	}
+	return "Description not available"
 }
