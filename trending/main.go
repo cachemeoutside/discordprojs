a3f35d1e1e8917472139f3e07a567295fa8df0e8
Anthony Vuong <vuonga91@gmail.com>
2021-09-08 23:13:49 -0700

    format code

--- trending/main.go
+++ trending/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"golang.org/x/net/html"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
-  "encoding/json"
-  "bytes"
-  "os"
 )
 
 type Message struct {
@@ -18,37 +18,37 @@ type Message struct {
 }
 
 type Payload struct {
-  Username string `json:"username"`
-  Avatar_url string `json:"avatar_url"`
-  Content string `json:"content"`
-  Embeds [1]Embed `json:"embeds"`
+	Username   string   `json:"username"`
+	Avatar_url string   `json:"avatar_url"`
+	Content    string   `json:"content"`
+	Embeds     [1]Embed `json:"embeds"`
 }
 
 type Embed struct {
-  Author Author `json:"author"`
-  Url string `json:"url"`
-  Title string `json:"title"`
-  Description string `json:"description"`
-  Color int `json:"color"`
-  Fields []Field `json:"fields"`
-  Footer Footer `json:"footer"`
+	Author      Author  `json:"author"`
+	Url         string  `json:"url"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Color       int     `json:"color"`
+	Fields      []Field `json:"fields"`
+	Footer      Footer  `json:"footer"`
 }
 
 type Footer struct {
-  Text string `json:"text"`
-  Icon_url string `json:"icon_url"`
+	Text     string `json:"text"`
+	Icon_url string `json:"icon_url"`
 }
 
 type Author struct {
-  Name string `json:"name"`
-  Url string `json:"url"`
-  Icon_url string `json:"icon_url"`
+	Name     string `json:"name"`
+	Url      string `json:"url"`
+	Icon_url string `json:"icon_url"`
 }
 
 type Field struct {
-  Name string `json:"name"`
-  Value string `json:"value"`
-  Inline string `json:"inline"`
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline string `json:"inline"`
 }
 
 func main() {
@@ -61,54 +61,54 @@ func main() {
 	}
 
 	result := make([]Message, len(urls))
-  f := make([]Field, len(urls))
+	f := make([]Field, len(urls))
 	for i, _ := range result {
 		result[i] = <-c
-    f[i] = Field{
-      Name: result[i].Project,
-      Value: result[i].About,
-      Inline: "false",
-    }
-  }
+		f[i] = Field{
+			Name:   result[i].Project,
+			Value:  result[i].About,
+			Inline: "false",
+		}
+	}
 
-  emb := Embed{
-    Author: Author{
-      Name: "Trending Bot",
-      Url: "https://google.com",
-      Icon_url: "https://i.imgur.com/R66g1Pe.jp",
-    },
-    Url: "https://github.com",
-    Title: "Trending Topics",
-    Description: "Automated messages",
-    Color: 15258703,
-    Fields: f,
-    Footer: Footer{
-      Text: "Brought to you by cachemeoutside",
-      Icon_url: "https://i.imgur.com/R66g1Pe.jpg",
-    },
-  }
+	emb := Embed{
+		Author: Author{
+			Name:     "Trending Bot",
+			Url:      "https://google.com",
+			Icon_url: "https://i.imgur.com/R66g1Pe.jp",
+		},
+		Url:         "https://github.com",
+		Title:       "Trending Topics",
+		Description: "Automated messages",
+		Color:       15258703,
+		Fields:      f,
+		Footer: Footer{
+			Text:     "Brought to you by cachemeoutside",
+			Icon_url: "https://i.imgur.com/R66g1Pe.jpg",
+		},
+	}
 
-  payload := Payload{
-    Username: "Trending Bot",
-    Avatar_url: "https://i.imgur.com/R66g1Pe.jpg",
-    Content: "Content",
-    Embeds: [1]Embed{emb},
-  }
+	payload := Payload{
+		Username:   "Trending Bot",
+		Avatar_url: "https://i.imgur.com/R66g1Pe.jpg",
+		Content:    "Content",
+		Embeds:     [1]Embed{emb},
+	}
 
-  b, err := json.Marshal(payload)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+	b, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-  resp, err := http.Post(os.Getenv("DISCORD_TRENDING_WEBHOOK"), "application/json", bytes.NewBuffer(b))
+	resp, err := http.Post(os.Getenv("DISCORD_TRENDING_WEBHOOK"), "application/json", bytes.NewBuffer(b))
 
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-  fmt.Println(resp)
+	fmt.Println(resp)
 }
 
 func getTrending() []string {
@@ -158,10 +158,10 @@ func getOutput(url string, c chan Message) {
 				if a.Key == "class" && a.Val == "f4 mt-3" {
 					tokenType := tokenizer.Next()
 					if tokenType == html.TextToken {
-            v := strings.TrimSpace(tokenizer.Token().Data)
-            if v != "" {
-  						msg.About = v
-            }
+						v := strings.TrimSpace(tokenizer.Token().Data)
+						if v != "" {
+							msg.About = v
+						}
 					}
 				}
 			}
